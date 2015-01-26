da5a6e85f9d8d33211ae02320a67b0f1cabffdfc
Golo Roden <webmaster@goloroden.de>
2015-01-26 11:25:47 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # defekt
+
 defekt is custom errors made simple.
+
+## Installation
+
+    $ npm install defekt
+
+## Quick start
+
+First you need to add a reference to defekt in your application.
+
+```javascript
+var defekt = require('defekt');
+```
+
+Then call the `defekt` function and hand over an array of custom error names that you would like to have created.
+
+```javascript
+var errors = defekt([
+  'ArgumentNull',
+  'InvalidOperation',
+  '...'
+]);
+```
+
+The result is an object containing all the errors you specified. To use one of those errors, simply call the appropriate function with `new` and throw it.
+
+```javascript
+throw new errors.InvalidOperation();
+```
+
+If you want to specify a message, feel free to do so.
+
+```javascript
+throw new errors.InvalidOperation('Something failed.');
+```
+
+Additionally, if an error was caused by another error, you can specify this error as the second parameter.
+
+```javascript
+throw new errors.InvalidOperation('Something failed.', new Error(...));
+```
+
+The custom errors follow the same rules as the built-in ones, i.e. they have a `name` and a `message` property, they derive from `Error` and they can be recognized by the `util.isError` function.
+
+## Running the build
+
+This module can be built using [Grunt](http://gruntjs.com/). Besides running the tests, this also analyses the code. To run Grunt, go to the folder where you have installed defekt and run `grunt`. You need to have [grunt-cli](https://github.com/gruntjs/grunt-cli) installed.
+
+    $ grunt
+
+## License
+
+The MIT License (MIT)
+Copyright (c) 2015 the native web.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/defektTests.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var util = require('util');
+
+var assert = require('assertthat');
+
+var defekt = require('../lib/defekt');
+
+suite('defekt', function () {
+  test('is a function.', function (done) {
+    assert.that(defekt, is.ofType('function'));
+    done();
+  });
+
+  test('throws an error if error names are missing.', function (done) {
+    assert.that(function () {
+      defekt();
+    }, is.throwing('Error names are missing.'));
+    done();
+  });
+
+  test('returns an object.', function (done) {
+    assert.that(defekt([ 'InvalidOperation' ]), is.ofType('object'));
+    done();
+  });
+
+  suite('errors', function () {
+    test('contains the specified errors.', function (done) {
+      var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+
+      assert.that(errors.InvalidOperation, is.ofType('function'));
+      assert.that(errors.ArgumentNull, is.ofType('function'));
+      done();
+    });
+
+    suite('CustomError', function () {
+      test('is an error.', function (done) {
+        var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+        var error = new errors.InvalidOperation();
+
+        assert.that(error, is.instanceOf(Error));
+        done();
+      });
+
+      test('is recognized by util.isError.', function (done) {
+        var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+        var error = new errors.InvalidOperation();
+
+        assert.that(util.isError(error), is.true());
+        done();
+      });
+
+      suite('name', function () {
+        test('contains the given name.', function (done) {
+          var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+          var error = new errors.InvalidOperation();
+
+          assert.that(error.name, is.equalTo('InvalidOperation'));
+          done();
+        });
+      });
+
+      suite('message', function () {
+        test('contains an empty string if no message was given.', function (done) {
+          var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+          var error = new errors.InvalidOperation();
+
+          assert.that(error.message, is.equalTo(''));
+          done();
+        });
+
+        test('contains the given message.', function (done) {
+          var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+          var error = new errors.InvalidOperation('foobar');
+
+          assert.that(error.message, is.equalTo('foobar'));
+          done();
+        });
+      });
+
+      suite('cause', function () {
+        test('is undefined if no inner error is given.', function (done) {
+          var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+          var error = new errors.InvalidOperation('foobar');
+
+          assert.that(error.cause, is.undefined());
+          done();
+        });
+
+        test('contains the given inner error.', function (done) {
+          var errors = defekt([ 'InvalidOperation', 'ArgumentNull' ]);
+          var cause = new errors.ArgumentNull();
+          var error = new errors.InvalidOperation('foobar', cause);
+
+          assert.that(error.cause, is.equalTo(cause));
+          done();
+        });
+      });
+    });
+  });
+});
