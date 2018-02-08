86796f771e90c0ee1fd65acfa4a806b34b720f87
Golo Roden <golo.roden@thenativeweb.io>
2018-02-08 19:47:16 +0100

    Release 1.0.0.

--- dist/defekt.js
+++ dist/defekt.js
@@ -2,7 +2,11 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-var inherits = require('inherits');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var defekt = function defekt(errorDefinitions) {
   if (!errorDefinitions) {
@@ -28,16 +32,26 @@ var defekt = function defekt(errorDefinitions) {
         throw new Error('Invalid error definition.');
     }
 
-    var CustomError = function CustomError(message, cause) {
-      Reflect.apply(Error, this, []);
+    var CustomError = function (_Error) {
+      _inherits(CustomError, _Error);
 
-      this.name = errorName;
-      this.code = errorCode;
-      this.message = message || '';
-      this.cause = cause;
-    };
+      function CustomError() {
+        var message = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+        var cause = arguments[1];
 
-    inherits(CustomError, Error);
+        _classCallCheck(this, CustomError);
+
+        var _this = _possibleConstructorReturn(this, (CustomError.__proto__ || Object.getPrototypeOf(CustomError)).call(this));
+
+        _this.name = errorName;
+        _this.code = errorCode;
+        _this.message = message;
+        _this.cause = cause;
+        return _this;
+      }
+
+      return CustomError;
+    }(Error);
 
     errors[errorName] = CustomError;
   });
