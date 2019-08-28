8c5e4be3f73c33995947b036e8e49c742ad55eef
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-28 11:22:46 +0200

    Migrate to TypeScript.

---
+++ lib/defekt.ts
@@ -0,0 +1,53 @@
+import humanizeString from 'humanize-string';
+
+export interface CustomError extends Error {
+  name: string;
+  code: string;
+  message: string;
+  cause?: Error;
+}
+
+type ErrorConstructors<TError> = {
+  [TKey in keyof TError]: new(message?: string, cause?: Error) => CustomError
+};
+
+const defekt = function <TErrorDefinition extends {
+  [ key: string ]: { code?: string };
+}> (errorDefinitions: TErrorDefinition): ErrorConstructors<TErrorDefinition> {
+  const errors: Partial<ErrorConstructors<TErrorDefinition>> = {};
+
+  /* eslint-disable guard-for-in */
+  for (const errorName in errorDefinitions) {
+    const errorDefinition = errorDefinitions[errorName];
+
+    if (!errorDefinition) {
+      continue;
+    }
+
+    const { code = `E${errorName.toUpperCase()}` } = errorDefinition;
+
+    errors[errorName] = class extends Error implements CustomError {
+      public name: string;
+
+      public code: string;
+
+      public message: string;
+
+      public cause?: Error;
+
+      public constructor (message = `${humanizeString(errorName)}.`, cause?: Error) {
+        super();
+
+        this.name = errorName;
+        this.code = code;
+        this.message = message;
+        this.cause = cause;
+      }
+    };
+  }
+  /* eslint-enable guard-for-in */
+
+  return errors as ErrorConstructors<TErrorDefinition>;
+};
+
+export default defekt;
