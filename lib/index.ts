13919c21fbdd39e6270355dd8551d2766181a24a
Golo Roden <golo.roden@thenativeweb.io>
2020-11-26 14:16:18 +0100

    feat: Add isError function.

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,11 @@
 import { CustomError } from './CustomError';
 import { defekt } from './defekt';
 import { isCustomError } from './isCustomError';
+import { isError } from './isError';
 
 export {
   CustomError,
   defekt,
-  isCustomError
+  isCustomError,
+  isError
 };
