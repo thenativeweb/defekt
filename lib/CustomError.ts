8cf668a934ede9fbaaf17a67179092f69cfb3b02
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2022-03-23 14:17:54 +0100

    fix: Adjust CustomError.cause type to changes in TypeScript.
    BREAKING CHANGE:
    This restricts the type of `cause` to `Error | undefined`.

--- lib/CustomError.ts
+++ lib/CustomError.ts
@@ -1,14 +1,12 @@
 class CustomError<TErrorName extends string = string> extends Error {
   public code: TErrorName;
 
-  public cause?: unknown;
-
   public data?: any;
 
   public constructor ({ code, message, cause, data }: {
     code: TErrorName;
     message: string;
-    cause?: unknown;
+    cause?: Error;
     data?: any;
   }) {
     super(message);
--- lib/CustomErrorConstructor.ts
+++ lib/CustomErrorConstructor.ts
@@ -2,7 +2,7 @@ import { CustomError } from './CustomError';
 
 interface CustomErrorConstructor<TErrorName extends string> {
   new(
-    parameters?: string | { cause?: unknown; data?: any; message?: string }
+    parameters?: string | { cause?: Error; data?: any; message?: string }
   ): CustomError<TErrorName>;
 
   code: string;
